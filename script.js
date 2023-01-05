12474c231415fc1711871119441684c104ab9fa9
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-01-05 22:47:07 +0100

    Add a slider that changes the grid size
    Every time the slider is triggered, the previous grid will be removed before
    adding the next one, which is important to avoid overlapping. Doing this
    with a single line of code making the innerHTML of the container blank
    was an important discovery.

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 const container = document.querySelector('.container');
+const slider = document.querySelector('.slider');
 
-let a = 16; // side size
+slider.addEventListener('input', function() {
+    console.log(event.target.value);
+    let gridSize = event.target.value;
+    container.innerHTML = '';
+    generateGrid(gridSize);
+})
 
-// creates squares
-for (let i = 0; i < a**2; i++) { 
-    let square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+// button prompts grid size
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+    let gridSize = prompt('Enter grid size: '); 
+    container.innerHTML = ''; // deletes previous grid before adding new one
+    generateGrid(gridSize);
+});
+
+// function generates grid
+function generateGrid(a) {
+    // creates squares
+    for (let i = 0; i < a**2; i++) { 
+        let square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    };
+
+    // selects all squares and adds them to array
+    let square = document.querySelectorAll('.square');
+
+    // finds the size the squares based on the side size and dimentions of the container
+    squareSize = {
+        height: `${500/a}px`,
+        width: `${500/a}px`,
+    };
+
+    for (let i = 0; i < a**2; i++) {
+        Object.assign(square[i].style, squareSize); // add size to each square
+
+        // add infinite hover for each square
+        square[i].addEventListener('mouseenter', function() {
+            square[i].classList.add('square-hover');
+        });
+    };
 };
 
-let square = document.querySelectorAll('.square');
 
-// finds the size the squares based on the side size and dimentions of the container
-squareSize = {
-    height: `${500/a}px`,
-    width: `${500/a}px`,
-};
 
-for (let i = 0; i < a**2; i++) {
-    Object.assign(square[i].style, squareSize); // add size to each square
 
-    // add infinite hover for each square
-    square[i].addEventListener('mouseenter', function() {
-        square[i].classList.add('square-hover');
-    });
-};
 
