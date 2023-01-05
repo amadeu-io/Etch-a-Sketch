124545eba246a3cf89ebb8b6bf59ca93447698dd
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-01-05 17:57:57 +0100

    Add drawing capability
    Adds infinite hover that changes the background color of the squares inside the for
    loop that iterates through all of them

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ let a = 16; // side size
 
 // creates squares
 for (let i = 0; i < a**2; i++) { 
-    const square = document.createElement('div');
+    let square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
-}
+};
 
-// selects all created squares and adds the squareSize style to every single one of them
-const square = document.querySelectorAll('.square');
+let square = document.querySelectorAll('.square');
 
+// finds the size the squares based on the side size and dimentions of the container
 squareSize = {
     height: `${500/a}px`,
     width: `${500/a}px`,
 };
 
 for (let i = 0; i < a**2; i++) {
-    Object.assign(square[i].style, squareSize); 
+    Object.assign(square[i].style, squareSize); // add size to each square
+
+    // add infinite hover for each square
+    square[i].addEventListener('mouseenter', function() {
+        square[i].classList.remove('square');
+        square[i].classList.add('square-hover');
+    });
 };
+
